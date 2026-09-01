036515f26651608004d5c6979f43177007f8a909
Saumy05 <saumy2475@gmail.com>
2026-09-01 14:45:01 +0530

    feat: add restaurant discount route to admin router

--- Frontend/src/modules/Food/components/admin/AdminRouter.jsx
+++ Frontend/src/modules/Food/components/admin/AdminRouter.jsx
@@ -33,6 +33,7 @@ const RestaurantComplaints = lazy(() => import("@food/pages/admin/restaurant/Res
 const RestaurantReviews = lazy(() => import("@food/pages/admin/restaurant/RestaurantReviews"));
 const RestaurantsBulkImport = lazy(() => import("@food/pages/admin/restaurant/RestaurantsBulkImport"));
 const RestaurantsBulkExport = lazy(() => import("@food/pages/admin/restaurant/RestaurantsBulkExport"));
+const RestaurantDiscount = lazy(() => import("@food/pages/admin/restaurant/RestaurantDiscount"));
 // Food Management
 const FoodsList = lazy(() => import("@food/pages/admin/foods/FoodsList"));
 const AddonsList = lazy(() => import("@food/pages/admin/addons/AddonsList"));
@@ -203,6 +204,7 @@ export default function AdminRouter() {
             <Route path="restaurants/reviews" element={<RestaurantReviews />} />
             <Route path="restaurants/bulk-import" element={<RestaurantsBulkImport />} />
             <Route path="restaurants/bulk-export" element={<RestaurantsBulkExport />} />
+            <Route path="restaurants/discount" element={<RestaurantDiscount />} />
 
             {/* FOOD & CATEGORY MANAGEMENT */}
             <Route path="categories" element={<Category />} />
